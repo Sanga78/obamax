063fe0d1e2232df7a01dbc9e4af8a3ecdf98300b
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-07-19 22:02:01 +0300

    Update views.py
    created views to list the products

--- store/views.py
+++ store/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Product
 
 # Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    return render(request,"store/product_list.html")
+
+def product_detail(request,product_id):
+    product = get_object_or_404(Product,id=product_id)
