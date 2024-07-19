cf14921c6cb3ced3e7b9419f1a219dd9accf3041
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-07-19 22:01:37 +0300

    Update models.py
    created 3 models(Product,Accesory and Category)

--- store/models.py
+++ store/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    stock = models.IntegerField()
+    image = models.ImageField(upload_to='products/')
+
+    def __str__(self):
+        return self.name    
+    
+class Accesory(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    accesory_type = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    stock = models.IntegerField()
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.accesory_type
