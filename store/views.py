7cfd220683d0344b5e28209e18e71d42d9862456
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-07-20 21:40:11 +0300

    Update views.py
    completed product detail view

--- store/views.py
+++ store/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import get_object_or_404, render
-from .models import Product
+from .models import Accesory, Product
 
 # Create your views here.
 def product_list(request):
     products = Product.objects.all()
-    return render(request,"store/product_list.html")
+    return render(request,"index.html",{'products':products})
 
 def product_detail(request,product_id):
     product = get_object_or_404(Product,id=product_id)
+    accesories = Accesory.objects.filter(product=product)
+    context = {
+        'product' : product,
+        'accesories' : accesories
+    }
+    return render(request,"product_detail.html", context)
