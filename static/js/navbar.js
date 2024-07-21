909ae060c1f60785331fb5a6cda1745eac66fd00
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-07-21 22:02:20 +0300

    Update navbar.js
    updated navbar.js ,cleaned the code

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,19 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const cartCount = document.getElementById('cart-total');
-    const searchBar = document.getElementById('search-bar');
-    const searchButton = document.getElementById('search-button');
     const menuIcon = document.getElementById('menu-btn');
     const nav = document.querySelector('header nav');
-
-    function updateCartCount() {
-        const cart = JSON.parse(localStorage.getItem('cart')) || [];
-        cartCount.textContent = cart.reduce((acc, item) => acc + item.quantity, 0);
-    }
-
-    searchButton.addEventListener('click', () => {
-        const query = searchBar.value.toLowerCase();
-        window.location.href = `products.html?search=${query}`;
-    });
 
     menuIcon.addEventListener('click', () => {
         nav.classList.toggle('active');
@@ -23,7 +10,4 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#search-btn').onclick = () => {
         searchForm.classList.toggle('active');
     }
-
-
-    updateCartCount();
 });
