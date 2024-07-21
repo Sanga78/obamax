2d81f02c62e56052339e2ab5042c5eb3e3db1e6f
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-07-21 22:00:44 +0300

    Update settings.py
    added default login url

--- Ecomart/settings.py
+++ Ecomart/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+
+from django.urls import reverse_lazy
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,3 +129,4 @@ MEDIA_ROOT = 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+LOGIN_URL = reverse_lazy('login')
