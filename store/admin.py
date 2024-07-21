a14ad87436e46f1b1a02b5165cfd2b212abdf951
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-07-21 22:03:50 +0300

    models
    created order ,shipping and cart models

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Accesory, Category, Product
+from .models import Accesory, Category, Order, OrderItem, Product
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Accesory)
+admin.site.register(Order)
+admin.site.register(OrderItem)
