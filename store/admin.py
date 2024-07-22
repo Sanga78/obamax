fcb6965356a62e49b7354f53fdb1e6e95e32ef4b
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-07-22 12:58:06 +0300

    redirect
    changed the home_page url path

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Accesory, Category, Order, OrderItem, Product
+from .models import Accesory, Category, Customer, Order, OrderItem, Product
 
 # Register your models here.
 admin.site.register(Category)
@@ -8,3 +8,4 @@ admin.site.register(Product)
 admin.site.register(Accesory)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+admin.site.register(Customer)
