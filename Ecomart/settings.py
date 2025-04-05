acdaf2b9d76e77de3fc580b62936c2bc6e5944c3
sanga78 <kelvinkipkosgaeisanga@gmail.com>
2025-04-05 12:11:23 +0300

    Update settings.py
    updates

--- Ecomart/settings.py
+++ Ecomart/settings.py
@@ -130,8 +130,8 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-MEDIA_URL ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
+MEDIA_URL ='media/products/'
+MEDIA_ROOT = 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
