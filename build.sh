6126c6cf57b6dfa28eaeebf0db2d4d29cd4b8116
sanga78 <kelvinkipkosgaeisanga@gmail.com>
2025-04-08 09:09:04 +0300

    Update build.sh
    updates

--- build.sh
+++ build.sh
@@ -10,3 +10,6 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+# Create superuser
+python manage.py createsuperuser --username "$DJANGO_SUPERUSER_USERNAME" --email "$DJANGO_SUPERUSER_EMAIL" --password "$DJANGO_SUPERUSER_PASSWORD"
