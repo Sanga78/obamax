4200eaccfa4abf98ca231c00adf251be9654e223
sanga78 <kelvinkipkosgaeisanga@gmail.com>
2025-04-09 08:35:24 +0300

    Update settings.py
    updates static files url and media urls

--- Ecomart/settings.py
+++ Ecomart/settings.py
@@ -122,19 +122,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'static',
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-MEDIA_URL ='media/products/'
-MEDIA_ROOT = 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = reverse_lazy('login')
 os.environ['DJANGO_SUPERUSER_PASSWORD'] = os.getenv('SUPERUSER_PASSWORD')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
